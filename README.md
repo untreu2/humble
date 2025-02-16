c41bfd2c1ff74d5fdd1023eed41bbb850023b58b
emir yorulmaz <emiryorulmaz@ikmail.com>
2025-02-16 21:04:59 +0300

    Gruvbox theme

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Humble - Fasting App
 
 [![Available on the App Store](https://image.nostr.build/63a4490a9b6e7c9d5e7218d1199e3ed4209bfabd840469e5e1bfafec2d6c0841.png)](https://apps.apple.com/app/humble-fasting-app/id6557081711)
-
-Download the APK for Android: https://github.com/untreu2/humble/releases/tag/humble
--- lib/theme.dart
+++ lib/theme.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 ThemeData buildLightTheme() {
   return ThemeData.light().copyWith(
-    scaffoldBackgroundColor: const Color(0xFFFBF1C7),
+    scaffoldBackgroundColor: const Color(0xFFD5C4A1),
     colorScheme: const ColorScheme.light(
-      primary: Color(0xFF458588),
-      secondary: Color(0xFFB16286),
-      surface: Color(0xFFFBF1C7),
+      primary: Color(0xFF665C54),
+      secondary: Color(0xFF928374),
+      surface: Color(0xFFD5C4A1),
       error: Color(0xFFCC241D),
       onPrimary: Color(0xFFFBF1C7),
-      onSecondary: Color(0xFFFBF1C7),
-      onSurface: Color(0xFF282828),
+      onSecondary: Color(0xFF3C3836),
+      onSurface: Color(0xFF3C3836),
       onError: Color(0xFFFBF1C7),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: const Color(0xFF458588),
+        backgroundColor: const Color(0xFF665C54),
         foregroundColor: const Color(0xFFFBF1C7),
       ),
     ),
@@ -24,21 +24,21 @@ ThemeData buildLightTheme() {
 
 ThemeData buildDarkTheme() {
   return ThemeData.dark().copyWith(
-    scaffoldBackgroundColor: Colors.black,
+    scaffoldBackgroundColor: const Color(0xFF1D2021),
     colorScheme: const ColorScheme.dark(
-      primary: Colors.white,
-      secondary: Colors.white,
-      surface: Colors.black,
-      error: Colors.white,
-      onPrimary: Colors.black,
-      onSecondary: Colors.black,
-      onSurface: Colors.white,
-      onError: Colors.black,
+      primary: Color(0xFFBDAE93),
+      secondary: Color(0xFF665C54),
+      surface: Color(0xFF1D2021),
+      error: Color(0xFFFB4934),
+      onPrimary: Color(0xFF1D2021),
+      onSecondary: Color(0xFFD5C4A1),
+      onSurface: Color(0xFFD5C4A1),
+      onError: Color(0xFF1D2021),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.black,
+        backgroundColor: const Color(0xFF665C54),
+        foregroundColor: const Color(0xFF3C3836),
       ),
     ),
   );
